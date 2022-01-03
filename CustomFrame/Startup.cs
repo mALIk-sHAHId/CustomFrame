c3957afb6b2355700fa851fcc4208605615ed4eb
mALIk sHAHId <mALIk_BsHAHId@outlook.com>
2022-01-03 21:11:38 +0530

    Add project files.

---
+++ CustomFrame/Startup.cs
@@ -0,0 +1,59 @@
+﻿using ElectronNET.API;
+using ElectronNET.API.Entities;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace MyElectronMusicPlayer
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit
+        // https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request
+        // pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            Bootstrap();
+        }
+
+        public async void Bootstrap()
+        {
+            var options = new BrowserWindowOptions
+            {
+                //Width = 1200,
+                //Height = 600,
+                //MinWidth = 940,
+                //MinHeight = 560,
+                Frame = false,
+
+                WebPreferences = new WebPreferences
+                {
+                    ContextIsolation = true,
+                    DevTools = true,
+                    WebSecurity = false,
+                }
+            };
+            await Electron.WindowManager.CreateWindowAsync(options);
+        }
+    }
+}
